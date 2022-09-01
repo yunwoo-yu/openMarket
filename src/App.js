5a8d78c154db27321217f1a5570eddde007c7c7a
yunwoo-yu <skypnal12@naver.com>
2022-09-02 01:30:38 +0900

    검색창 작업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Home from "./pages/Home";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`${reset}
+*{box-sizing:border-box;}
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Home />
     </>
   );
