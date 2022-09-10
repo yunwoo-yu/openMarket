a32c1d3fd3468b8b5ec0fb5ec08d455f6335b19b
Ycow <skypnal12@gmail.com>
2022-09-10 13:48:40 +0900

    add: 수량버튼추가

--- src/components/Products/ProductsDetail/ProductsDetail.jsx
+++ src/components/Products/ProductsDetail/ProductsDetail.jsx
@@ -1,62 +1,43 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getProductsDetail } from "../../../axios-api";
 import { DefaultWrapper } from "../../UI/Layout/DefaultWrapper";
 import { InnerContainer } from "../../UI/Layout/InnerContainer";
 import { ProductDetail } from "./styled";
 
-const ProductsDetail = () => {
-  const [productData, setProductData] = useState({});
-  const [productAmount, setProductAmount] = useState(1);
-  const { id } = useParams();
-  const publicUrl = process.env.PUBLIC_URL;
-
-  const getPriceWon = () => {
-    if (productData.price) {
-      return productData.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-    return "";
-  };
-
-  const requestGetProductDetail = async () => {
-    try {
-      const response = await getProductsDetail(id);
-      setProductData(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    requestGetProductDetail();
-  }, []);
-
+const ProductsDetail = ({
+  onURL,
+  onData,
+  onPrice,
+  onAmount,
+  onPlus,
+  onMinus,
+}) => {
+  console.log();
   return (
     <DefaultWrapper>
       <InnerContainer>
         <ProductDetail>
           <figure>
-            <img src={productData.image} alt="상품 이미지" />
+            <img src={onData.image} alt="상품 이미지" />
             <figcaption>
-              <p className="store-name">{productData.seller_store}</p>
-              <h2 className="product-name">{productData.product_name}</h2>
+              <p className="store-name">{onData.seller_store}</p>
+              <h2 className="product-name">{onData.product_name}</h2>
               <p className="product-price">
-                {getPriceWon()}
+                {onPrice()}
                 <span>원</span>
               </p>
               <p className="parcel">
-                {productData.shipping_method} / {productData.shipping_fee}
+                {onData.shipping_method} / {onData.shipping_fee}
               </p>
               <div className="product-amount-btn">
-                <button>
+                <button onClick={onMinus}>
                   <img
-                    src={`${publicUrl}/assets/icon-minus-line.svg`}
+                    src={`${onURL}/assets/icon-minus-line.svg`}
                     alt="개수 빼기 버튼"
                   />
                 </button>
-                <span>1</span>
-                <button>
+                <span>{onAmount}</span>
+                <button onClick={onPlus}>
                   <img
-                    src={`${publicUrl}/assets/icon-plus-line.svg`}
+                    src={`${onURL}/assets/icon-plus-line.svg`}
                     alt="개수 추가 버튼"
                   />
                 </button>
@@ -65,10 +46,10 @@ const ProductsDetail = () => {
                 <p>총 상품 금액</p>
                 <p className="sum-amount">
                   총수량
-                  <span>{productAmount}</span>개
+                  <span>{onAmount}</span>개
                 </p>
                 <p className="sum-price">
-                  {(productData.price * productAmount)
+                  {(onData.price * onAmount)
                     .toString()
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                   <span>원</span>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,51 @@
-import { ProductDetail } from "../components/Products/ProductsDetail/styled";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProductsDetail } from "../axios-api";
+import ProductsDetail from "../components/Products/ProductsDetail/ProductsDetail";
 
 const Product = () => {
-  return <ProductDetail />;
+  const [productData, setProductData] = useState({});
+  const [productAmount, setProductAmount] = useState(1);
+
+  const { id } = useParams();
+  const publicUrl = process.env.PUBLIC_URL;
+
+  const amountPlus = () => {
+    setProductAmount(productAmount + 1);
+  };
+  const amountMinus = () => {
+    setProductAmount(productAmount - 1);
+  };
+
+  const getPriceWon = () => {
+    if (productData.price) {
+      return productData.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    return "";
+  };
+
+  const requestGetProductDetail = async () => {
+    try {
+      const response = await getProductsDetail(id);
+      setProductData(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    requestGetProductDetail();
+  }, []);
+
+  return (
+    <ProductsDetail
+      onURL={publicUrl}
+      onData={productData}
+      onAmount={productAmount}
+      onSetAmount={setProductAmount}
+      onPrice={getPriceWon}
+      onPlus={amountPlus}
+      onMinus={amountMinus}
+    />
+  );
 };
 export default Product;
