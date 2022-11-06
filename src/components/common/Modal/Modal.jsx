1327435196864d3bd76522c14d7b2f3f94acb5cf
Ycow <skypnal12@gmail.com>
2022-11-06 23:38:11 +0900

    feat: 상품디테일 페이지 장바구니 모달창 구현

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -1,14 +1,24 @@
 import { ModalWrapper } from "./styled";
 import { Button } from "../Button/Button";
 
-const Modal = ({ children, rejectText1, resultText2 }) => {
+const Modal = ({
+  children,
+  rejectText,
+  resultText,
+  onClickReject,
+  onClickResult,
+}) => {
   return (
     <ModalWrapper>
-      <button className="modal-close-btn"></button>
+      <button className="modal-close-btn" onClick={onClickReject}></button>
       <p className="modal-text">{children}</p>
       <div className="modal-btn-box">
-        <Button className="reject-btn">{rejectText1}</Button>
-        <Button className="result-btn">{resultText2}</Button>
+        <Button className="reject-btn" onClick={onClickReject}>
+          {rejectText}
+        </Button>
+        <Button className="result-btn" onClick={onClickResult}>
+          {resultText}
+        </Button>
       </div>
     </ModalWrapper>
   );
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,39 +1,54 @@
 import ProductsDetail from "../../components/Products/ProductsDetail/ProductsDetail.jsx";
 import { useMutation, useQuery } from "react-query";
-import { useParams } from "react-router-dom";
-import { getProductsDetail, postCartItem } from "../../lib/api/axios-api.js";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import {
+  getProductsDetail,
+  getUserCart,
+  postCartItem,
+} from "../../lib/api/axios-api.js";
+import Modal from "../../components/common/Modal/Modal";
 import { useState } from "react";
 import Loading from "../../components/common/Loading/Loading.jsx";
 
 const ProductPage = () => {
   const [amount, setAmount] = useState(1);
+  const [isModal, setIsModal] = useState(false);
+  const navigate = useNavigate();
   const { id } = useParams();
   const { data, isLoading, isError, error } = useQuery(["products", id], () =>
     getProductsDetail(id)
   );
 
+  const { data: cartData } = useQuery("cart", getUserCart);
   const mutation = useMutation(postCartItem, {
     onSuccess(data) {
       console.log(data);
+      setIsModal(true);
     },
     onError(err) {
       console.log(err);
     },
   });
+  const isCartItemCheck = () => {
+    const isCartItem = cartData.filter(
+      (item) => item.product_id === data.product_id
+    ).length;
 
-  console.log(data);
+    return isCartItem;
+  };
 
   const onClickAddProductToCart = () => {
     mutation.mutate({
       product_id: data.product_id,
       quantity: amount,
-      check: true,
+      check: isCartItemCheck() ? true : false,
     });
   };
 
   const onClickPlus = () => {
     setAmount((prev) => prev + 1);
   };
+
   const onClickMinus = () => {
     if (amount > 1) {
       setAmount((prev) => prev - 1);
@@ -41,12 +56,8 @@ const ProductPage = () => {
   };
 
   if (isLoading) return <Loading />;
-  if (isError)
-    return (
-      <>
-        <p>{error.response.data.detail}</p>
-      </>
-    );
+  if (isError) return <p>{error.response.data.detail}</p>;
+
   const convetedPrice = data.price
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -57,6 +68,34 @@ const ProductPage = () => {
 
   return (
     <>
+      {isModal &&
+        (isCartItemCheck() ? (
+          <Modal
+            rejectText={"아니요"}
+            resultText={"예"}
+            onClickReject={() => {
+              setIsModal(false);
+            }}
+            onClickResult={() => {
+              navigate("/cart");
+            }}
+          >
+            이미 장바구니에 있는 상품입니다. 장바구니로 이동하시겠습니까?
+          </Modal>
+        ) : (
+          <Modal
+            rejectText={"아니요"}
+            resultText={"예"}
+            onClickReject={() => {
+              setIsModal(false);
+            }}
+            onClickResult={() => {
+              navigate("/cart");
+            }}
+          >
+            장바구니에 상품이 담겼습니다. 장바구니로 이동하시겠습니까?
+          </Modal>
+        ))}
       <ProductsDetail
         convetedPrice={convetedPrice}
         convertedSum={convertedSum}
